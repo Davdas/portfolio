b24df04cb2b4aab430437b385cd39ea6054acb35
Davdas <davide_mancini82@yahoo.it>
2024-04-07 01:09:49 +0200

    fatta pagina contatti

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.querySelector(".contact-form");
+  const submitButton = document.querySelector("#button-form");
+  const nameInput = document.querySelector("#name");
+  const emailInput = document.querySelector("#email");
+  const objectInput = document.querySelector("#object");
+  const messageInput = document.querySelector("#message");
+
+  const publicKey = "8DE9mU0Jlc6rfujCZ";
+  const serviceID = "service_kb29hka";
+  const templateID = "template_keowvem";
+
+  emailjs.init(publicKey);
+
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const inputField = {
+      name: nameInput.value,
+      email: emailInput.value,
+      object: objectInput.value,
+      message: messageInput.value,
+    };
+
+    emailjs.send(serviceID, templateID, inputField).then(() => {
+      submitButton.innerText = "Messaggio Inviato";
+      nameInput.value = "";
+      emailInput.value = "";
+      objectInput.value = "";
+      messageInput.value = "";
+      alert("Messaggio inviato correttamente");
+      // Ritorna al testo originale del pulsante dopo un breve ritardo
+      setTimeout(function () {
+        submitButton.innerText = "Invia";
+      }, 2000);
+    });
+  }),
+    (error) => {
+      console.log(error);
+      submitButton.innerText = "Messaggio NON Inviato";
+      alert("Si è verificato un errore durante l'invio del messaggio.");
+    };
+});
+
 //menu toggle
 let hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
