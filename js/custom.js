cf7bdda5087113cc64fb8acbdf20ff7da714dc9e
Davdas <davide_mancini82@yahoo.it>
2024-04-06 16:58:57 +0200

    ultime rifiniture

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,14 @@ let hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
   document.body.classList.toggle("menu-open");
 });
+
+// change navbar color
+window.addEventListener("scroll", function () {
+  let header = document.querySelector(".header");
+
+  if (window.scrollY > 100) {
+    header.style.backgroundColor = "#f5dfd0";
+  } else {
+    header.style.backgroundColor = "#f8e7dc";
+  }
+});
